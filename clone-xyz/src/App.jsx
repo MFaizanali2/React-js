69882c0f9d3378464e79597060f82b9efc5052f7
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-09-09 07:23:59 +0500

    Update App.jsx

--- clone-xyz/src/App.jsx
+++ clone-xyz/src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css'
-import Header from "g:/React-js/clone-xyz/src/components/header/header";
-import Footer from "g:/React-js/clone-xyz/src/components/footer/footer";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
 const app = () => {
   return(
 
