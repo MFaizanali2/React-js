3773cb242abfb2352f5d58645548f87a10250bc7
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-09-14 23:24:53 +0500

    Update header.jsx

--- clone-xyz/src/components/header/header.jsx
+++ clone-xyz/src/components/header/header.jsx
@@ -1,9 +1,9 @@
 
-// import './header.css'
-// function Header (){
-//     return (
-//         <h1 className='h1'>header</h1>
-//     )
-// }
+import './header.css'
+function Header (){
+    return (
+        <h1 className='h1'>header</h1>
+    )
+}
 
-// export default Header;
+export default Header;
