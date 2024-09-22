2713d88e9eb3fb30c4b8f6d9e0061a78e75966b1
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-09-22 23:54:39 +0500

    Update footer.jsx

--- clone-xyz/src/components/footer/footer.jsx
+++ clone-xyz/src/components/footer/footer.jsx
@@ -1,12 +1,12 @@
 
-import Button from '../comman/button/Button'
-import './footer.css'
-function Footer (){
-    return(
-        <>
-        <h1>footer</h1>
-        <Button />
-        </>
-    )
-}
-export default Footer;
+// import Button from '../comman/button/Button'
+// import './footer.css'
+// function Footer (){
+//     return(
+//         <>
+//         <h1>footer</h1>
+//         <Button />
+//         </>
+//     )
+// }
+// export default Footer;
