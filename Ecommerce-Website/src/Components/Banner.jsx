54c70755f8ee3fb937bbe3f56d8b12523c18040e
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-10-23 18:15:49 +0500

    code :construction:

--- Ecommerce-Website/src/Components/Banner.jsx
+++ Ecommerce-Website/src/Components/Banner.jsx
@@ -9,8 +9,8 @@ const Banner = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed: 4500,
+    autoplay: true,
+    autoplaySpeed: 4500,
   };
   return (
     <>
--- Ecommerce-Website/src/pages/Contact.jsx
+++ Ecommerce-Website/src/pages/Contact.jsx
@@ -1,10 +1,36 @@
+import { useForm } from "react-hook-form";
 import Header from "../Components/Header"
 
 const Contact = () => {
+  const { register, formState: { errors }, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
+   
   return (
     <div>
       <Header/>
-      Contact
+      <div className=" bg-gray-700 flex justify-center items-center p-7 ">
+      {/* <form className="flex flex-col items-center justify-center bg-slate-400 gap-5 w-1/2 p-3" onSubmit={handleSubmit(onSubmit)}>
+      <input className="placeholder:text-slate-600 text-lg"  {...register("firstName", { required: true, maxLength: 20 })} />
+      <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+      <input type="number" {...register("", { min: 18, max: 99 })} />
+      <input className="bg-green-400 w-3/5" type="submit" />
+    </form> */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <input 
+        {...register("firstName", { required: true })} 
+        aria-invalid={errors.firstName ? "true" : "false"} 
+      />
+      {errors.firstName?.type === 'required' && <p role="alert">First name is required</p>}
+
+      <input 
+        {...register("mail", { required: "Email Address is required" })} 
+        aria-invalid={errors.mail ? "true" : "false"} 
+      />
+      {errors.mail && <p role="alert">{errors.mail?.message}</p>}
+      
+      <input type="submit" />
+    </form>
+      </div>
     </div>
   )
 }
